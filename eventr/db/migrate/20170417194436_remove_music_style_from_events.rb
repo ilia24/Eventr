a826de8d3abc6abdf0af4d95d1d574e771202190
ilia24 <iliademertchian24@gmail.com>
2017-04-17 16:01:35 -0400

    Destroy bad migrations

--- eventr/db/migrate/20170417194436_remove_music_style_from_events.rb
+++
@@ -1,5 +0,0 @@
-class RemoveMusicStyleFromEvents < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :music_style
-  end
-end
--- eventr/db/migrate/20170417194600_remove_venue_style_from_events.rb
+++
@@ -1,5 +0,0 @@
-class RemoveVenueStyleFromEvents < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :venue_style
-  end
-end
