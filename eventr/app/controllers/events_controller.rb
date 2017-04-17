9a6ce5c028d50b293dd9499d835487f5852826fd
pritam-das <pritam.das@mail.utoronto.ca>
2017-04-17 15:48:37 -0400

    Set up the routes and the controller

--- eventr/app/controllers/events_controller.rb
+++ eventr/app/controllers/events_controller.rb
@@ -1,2 +1,47 @@
 class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      redirect_back_or_to @event
+    end
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to events_path
+    else
+      redirect_to new_event_path
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path
+  end
+
+  def event_params
+    params.require(:event).permit(:name, :data, :capacity)
+  end
+
 end
