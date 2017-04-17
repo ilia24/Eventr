d57b6e2a48f1ddf8205622634a744085a81b223c
ilia24 <iliademertchian24@gmail.com>
2017-04-17 16:39:31 -0400

    Add time field to events database

--- eventr/db/schema.rb
+++ eventr/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170417200453) do
+ActiveRecord::Schema.define(version: 20170417203522) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170417200453) do
     t.integer  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.time     "time"
   end
 
 end
