416c83c8a6e53888a40a0643739b5073926ca81a
pritam-das <pritam.das@mail.utoronto.ca>
2017-04-17 19:21:44 -0400

    just a push

--- eventr/app/models/event.rb
+++ eventr/app/models/event.rb
@@ -1,2 +1,7 @@
 class Event < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+
+    has_attached_file :avatar, styles: { medium: "300x300", thumb: "100x100" }
+    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
