54e8a9b02274a4d599dab6cac39461a99ff0c52d
ilia24 <iliademertchian24@gmail.com>
2017-04-19 14:56:33 -0400

    reworked routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'events#index'
-get 'events' => 'events#index'
 
 resources :events
 
