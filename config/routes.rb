27d94f69c9d2546b8d22a70c449c78a31b469090
pritam-das <pritam.das@mail.utoronto.ca>
2017-04-19 23:54:32 -0400

    added user controller and sessions controller for log in and out successfully

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'events#index'
 
 resources :events
+resources :users, only: %i(new create)
+resources :sessions, only: %i(new create destroy)
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419184256) do
+ActiveRecord::Schema.define(version: 20170420033533) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170419184256) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "email"
   end
 
 end
