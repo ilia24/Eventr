d725b6a256100782dc253e9c71ca9684217ccc2f
pritam-das <pritam.das@mail.utoronto.ca>
2017-04-20 00:01:39 -0400

    Added logic to display 'Signed in as..' or prompt to 'log in' or 'sign up' in the application view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    session[:user_id] && User.find(session[:user_id])
+  end
+
+  helper_method :current_user
 end
