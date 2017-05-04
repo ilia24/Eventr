f483433c66deccb093760e9aea9e97ca7ee7cb9f
ilia24 <iliademertchian24@gmail.com>
2017-05-04 17:06:48 -0400

    Add model for group

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420040358) do
+ActiveRecord::Schema.define(version: 20170504210604) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 20170420040358) do
     t.datetime "updated_at",  null: false
     t.time     "time"
     t.string   "event_style"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "reviews", force: :cascade do |t|
