ff91375c777f32e9eea11661d0cbdf6f52c9e3c3
pritam-das <pritam.das@mail.utoronto.ca>
2017-05-05 15:16:00 -0400

    Deleted hosts

--- app/models/host.rb
+++
@@ -1,5 +0,0 @@
-class Host < ApplicationRecord
-  has_secure_password
-  validates :email, :name, presence: true
-  has_many :events
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170505161942) do
+ActiveRecord::Schema.define(version: 20170504212215) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -44,10 +44,8 @@ ActiveRecord::Schema.define(version: 20170505161942) do
     t.string   "name"
     t.text     "description"
     t.integer  "capacity"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "email"
-    t.string   "password_digest"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "reviews", force: :cascade do |t|
