86576252a7ccabd779d49674c9fe2bef0853f777
pritam-das <pritam.das@mail.utoronto.ca>
2017-05-05 18:39:04 -0400

    on form 2 setup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,11 @@ class SessionsController < ApplicationController
 
   def create
     user= User.find_by(email: params[:email])
-    host= Host.find_by(email: params[:email])
 
     #if the login details belong to a user
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      flash[:notice] = "Logged in succesfully as #{user.name} !"
-      redirect_to events_url
-
-    #if the login details belong to a host
-    elsif host && host.authenticate(params[:password])
-      session[:host_id] = host.id
-      flash[:notice] = "Logged in succesfully as #{host.name}!"
+      flash[:notice] = "Logged in succesfully as #{user.email} !"
       redirect_to events_url
 
     else
@@ -26,17 +19,11 @@ class SessionsController < ApplicationController
 
   def destroy
     # if user is logged in, log out
-    if current_user
+
       session[:user_id] = nil
       flash[:notice] = 'User has been logged out succesfully!'
       redirect_to events_url
       #if host is logged in, log out
-    elsif current_host
-      session[:host_id] = nil
-      flash[:notice] = 'Host has been logged out succesfully!'
-      redirect_to events_url
-
-    end
 
   end
 end
