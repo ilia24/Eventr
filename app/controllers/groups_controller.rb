fc8f065ca687ef15d590af917db30520bda8728f
ilia24 <iliademertchian24@gmail.com>
2017-05-05 19:43:42 -0400

    Create base controler functions for groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,36 @@
 class GroupsController < ApplicationController
-  def show
+  before_action :load_user
+
+  def new
+    @group = Group.new
   end
+
 
   def create
+    @group = @event.groups.build(group_params)
+    @group.event = @event.id
+
+
+    if @eventrinfo.save
+      flash[:notice] = 'group created succesfully!'
+    else
+      render :new
+    end
   end
 
-  def destory
+  def update
   end
+
+  def destroy
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name)
+  end
+
+  def load_user
+    @event = Event.find(params[:event_id])
+  end
+
 end
