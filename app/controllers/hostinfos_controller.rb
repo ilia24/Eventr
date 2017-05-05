5e2d4b63e4c8810151dc59a7ad5e1c6a554aae63
pritam-das <pritam.das@mail.utoronto.ca>
2017-05-05 19:46:28 -0400

    Form 3 basic layout setup and running

--- app/controllers/hostinfos_controller.rb
+++ app/controllers/hostinfos_controller.rb
@@ -1,10 +1,36 @@
 class HostinfosController < ApplicationController
-  def create
-  end
+  before_action :load_user
 
-  def update
-  end
+    def new
+      @hostinfo = Hostinfo.new
+    end
 
-  def destroy
+    def create
+      # @hostinfo = @user.hostinfo.build(hostinfo_params)
+      @hostinfo = Hostinfo.new(hostinfo_params)
+      @hostinfo.user = current_user
+
+      if @hostinfo.save
+        flash[:notice] = 'Eventr profile updated succesfully!'
+        redirect_to events_url
+      else
+        render :new
+      end
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+    def hostinfo_params
+      params.require(:hostinfo).permit(:name, :address, :capacity, :website)
+    end
+
+    def load_user
+      @user = User.find(params[:user_id])
+    end
+
   end
-end
