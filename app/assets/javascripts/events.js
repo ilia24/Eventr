27bb9aec2a3a629637474f7baf49e2509fb1bd4c
ilia24 <iliademertchian24@gmail.com>
2017-05-08 02:00:33 -0400

    Tweaked map creation script to only run if an HTML objects exists with an ID of map

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,21 +1,20 @@
 $(function() {
 
-  console.log('THINGS HAS HAPPENED');
-  function initMap() {
+if($('#map').length) {
     var lat = parseFloat($('#map').attr('data-parameter1'));
     var lng = parseFloat($('#map').attr('data-parameter2'));
     var eventlocation = {lat, lng};
+
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 15,
       center: eventlocation
     });
+
     var marker = new google.maps.Marker({
       position: eventlocation,
       map: map,
       title: 'Event location',
       label: 'E'
     });
-  };
-
-  $('#map').onload = initMap();
+};
 });
