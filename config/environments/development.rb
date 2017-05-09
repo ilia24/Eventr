2575888f9eecda072cfb87e3cb5dac55b0f8b555
ilia24 <iliademertchian24@gmail.com>
2017-05-09 14:44:19 -0400

    Uninstall paperclip & install carrierwave

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  Paperclip.options[:command_path] = "/usr/bin/"
-  
+  # Paperclip.options[:command_path] = "/usr/bin/"
+
 end
