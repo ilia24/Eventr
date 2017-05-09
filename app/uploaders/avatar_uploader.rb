c22756dfa8e9483ba1a7445c041accff1026fb5a
ilia24 <iliademertchian24@gmail.com>
2017-05-09 16:07:32 -0400

    Sanitize avatar uploader file inputs

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -14,6 +14,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
     "avatar/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def extension_whitelist
+    %w(jpg jpeg png)
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
