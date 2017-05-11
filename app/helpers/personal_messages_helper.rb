233aa65624bf0905bdf63d385fc7171e7fa27455
pritam-das <pritam.das@mail.utoronto.ca>
2017-05-11 19:29:29 -0400

    One to one chat working properly, needs styling

---
+++ app/helpers/personal_messages_helper.rb
@@ -0,0 +1,2 @@
+module PersonalMessagesHelper
+end
---
+++ test/controllers/personal_messages_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PersonalMessagesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
