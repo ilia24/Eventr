02801b39b7a017860b96f867594c535c2c48067d
ilia24 <iliademertchian24@gmail.com>
2017-05-11 20:38:30 -0400

    Fix database migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170511200642) do
+ActiveRecord::Schema.define(version: 20170512003748) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "author_id"
