84a32b094abc7f514d493eb42a320eb9c95888da
Brian Du <brian.bh.du@gmail.com>
2017-05-12 16:46:51 -0400

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,24 +14,6 @@
 //= require jquery_ujs
 //= require_tree .
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
 $(document).ready(function(){
   $('.owl-carousel').owlCarousel({
     stagePadding: 100,
