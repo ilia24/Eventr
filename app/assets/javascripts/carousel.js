bebfc80d908f804fd323b5e3d7daaba9faccbb51
ilia24 <iliademertchian24@gmail.com>
2017-05-12 21:59:53 -0400

    Delete stray js, explicity require js files in application and add z-index fix to dropdown

---
+++ app/assets/javascripts/carousel.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    stagePadding: 100,
+    loop:true,
+    margin:0,
+    nav:true,
+    center:true,
+    autoWidth:true,
+    autoHeight:true,
+    autoplay:true,
+    autoplayTimeout:4000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:3
+        }
+    }
+  })
+});
