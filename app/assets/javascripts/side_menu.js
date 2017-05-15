8b82307ba6940c44a240b04abf5c10069a7bee1a
Brian Du <brian.bh.du@gmail.com>
2017-05-15 19:00:28 -0400

    Added collapse to event details in group chat

--- app/assets/javascripts/side_menu.js
+++ app/assets/javascripts/side_menu.js
@@ -21,7 +21,7 @@ $(function() {
     $('.side_menu_group_content').toggleClass( "group_slide_out" );
   });
 
-  // Collapses groups section
+  // Collapses and reveals groups section
   $('#groups').on('click', function(e){
 
     $(this).next('.side_menu_main_groups_list').slideToggle('fast');
@@ -31,11 +31,22 @@ $(function() {
 
   });
 
+  // Collapses and reveals eventr section
   $('#eventrs').on('click', function(e){
 
     $(this).next('.side_menu_main_eventrs_list').slideToggle('fast');
     $(this).toggleClass('active');
     $(this).find('i').toggleClass('fa-chevron-down fa-chevron-up')
+    e.preventDefault();
+
+  });
+
+  // Collapses and reveals event detail
+  $('.side_menu_group_event_details').hide();
+  $('.event_details_btn').on('click', function(e){
+
+    $('.side_menu_group_event_details').slideToggle('fast');
+    $(this).toggleClass('active');
     e.preventDefault();
 
   });
