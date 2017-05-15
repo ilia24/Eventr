10a564c2b3bc2ebcf4cafe52d28cfac6731390f3
Brian Du <brian.bh.du@gmail.com>
2017-05-15 17:28:05 -0400

    Added main menu groups section collapsible

--- app/assets/javascripts/side_menu.js
+++ app/assets/javascripts/side_menu.js
@@ -13,11 +13,20 @@ $(function() {
     $('.side_menu_wrapper').toggleClass( "slide_open" );
   });
 
+  // Goes back to main chat menu
   $('.side_menu_back_chat').on('click', function(e){
     e.preventDefault();
 
     $('.side_menu_main_content').toggleClass( "main_slide_in" );
     $('.side_menu_group_content').toggleClass( "group_slide_out" );
+  });
+
+  // Collapses groups section
+  $('#groups').on('click', function(e){
+
+    $(this).next('.side_menu_main_groups_list').slideToggle('fast');
+    $(this).toggleClass('active');
+    e.preventDefault();
 
   });
 
