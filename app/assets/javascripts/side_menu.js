4f437653b5df52c92222b6f5fa0f929ebff62d4c
Brian Du <brian.bh.du@gmail.com>
2017-05-15 09:15:45 -0400

    Added side menu main content slide in

--- app/assets/javascripts/side_menu.js
+++ app/assets/javascripts/side_menu.js
@@ -1,5 +1,6 @@
 $(function() {
 
+  // This open and closes the entire side menu
   $('.open_side_menu').on('click', function(e){
     e.preventDefault();
 
@@ -10,6 +11,12 @@ $(function() {
     e.preventDefault();
 
     $('.side_menu_wrapper').toggleClass( "slide_open" );
+  });
+
+  $('.side_menu_back_chat').on('click', function(e){
+    e.preventDefault();
+
+    $('.side_menu_main_content').toggleClass( "main_slide_in" );
   });
 
 });
