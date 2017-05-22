2bcdfe061cb33e73b9ecb701de88793906090915
pritam-das <pritam.das@mail.utoronto.ca>
2017-05-22 16:18:55 -0400

    Added the notifications broadcastjob to personalmessage.rb

--- app/models/personal_message.rb
+++ app/models/personal_message.rb
@@ -5,5 +5,10 @@ class PersonalMessage < ApplicationRecord
   belongs_to :author, class_name: "User"
   belongs_to :receiver, class_name: "User"
 
+  after_create_commit do
+    conversation.touch
+    NotificationBroadcastJob.perform_later(self)
+  end
+
 
 end
