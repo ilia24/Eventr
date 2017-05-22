eb2ebd0fa7f1c86dd0ece1a672f237eb6e3c35c9
pritam-das <pritam.das@mail.utoronto.ca>
2017-05-22 15:49:54 -0400

    Added has_many sent_personal messages and received personal messages for user, under class Personal Messages, with foreign key Author and Receiver Id

--- app/models/personal_message.rb
+++ app/models/personal_message.rb
@@ -2,6 +2,7 @@ class PersonalMessage < ApplicationRecord
 
   validates :body, presence: true
   belongs_to :conversation
-  belongs_to :user
+  belongs_to :author, class_name: "User"
+  belongs_to :receiver, class_name: "User"
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -27,8 +27,9 @@ class User < ApplicationRecord
 
   has_many :authored_conversations, class_name: 'Conversation', foreign_key: 'author_id'
   has_many :received_conversations, class_name: 'Conversation', foreign_key: 'receiver_id'
+  has_many :sent_personal_messages, class_name: "PersonalMessage", foreign_key: 'author_id', dependent: :destroy
+  has_many :received_personal_messages, class_name: "PersonalMessage", foreign_key: 'receiver_id', dependent: :destroy
 
-  has_many :personal_messages, dependent: :destroy
 
 
   def channel
