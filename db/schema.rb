55455263dbe6a3bcc747148dbd6ba4f8ac5ef049
pritam-das <pritam.das@mail.utoronto.ca>
2017-05-22 19:09:44 -0400

    Fixed personal messages schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170519022306) do
+ActiveRecord::Schema.define(version: 20170522230555) do
 
   create_table "comments", force: :cascade do |t|
     t.text    "content"
@@ -108,11 +108,13 @@ ActiveRecord::Schema.define(version: 20170519022306) do
   create_table "personal_messages", force: :cascade do |t|
     t.text     "body"
     t.integer  "conversation_id"
-    t.integer  "user_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "author_id"
+    t.integer  "receiver_id"
+    t.index ["author_id"], name: "index_personal_messages_on_author_id"
     t.index ["conversation_id"], name: "index_personal_messages_on_conversation_id"
-    t.index ["user_id"], name: "index_personal_messages_on_user_id"
+    t.index ["receiver_id"], name: "index_personal_messages_on_receiver_id"
   end
 
   create_table "requests", force: :cascade do |t|
