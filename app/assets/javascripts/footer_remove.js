66919b1368cdee8f60e15d93898537ff0ffd350a
Brian Du <brian.bh.du@gmail.com>
2017-05-24 18:00:08 -0400

    Removed footer from login and signup page

--- app/assets/javascripts/footer_remove.js
+++ app/assets/javascripts/footer_remove.js
@@ -1,6 +1,6 @@
 $(function() {
 
-  // Removes footer for login page
+  // Removes footer for login and signup page
   login = $('.log_sign_wrapper').parent();
   login.children('footer').remove();
 
