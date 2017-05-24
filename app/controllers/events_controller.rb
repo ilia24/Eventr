5ec418520aea46db4c686582f5ecc4f28493540a
pritam-das <pritam.das@mail.utoronto.ca>
2017-05-24 12:27:19 -0400

    Ensured that when a not logged in user tries to press going for an event, it leads to the user login page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :ensure_logged_in, only: [:new, :create, :destroy, :edit, :update]
+  before_action :ensure_logged_in, only: [:new, :create, :destroy, :edit, :update, :join]
   before_action :ensure_hostinfo_filled_out, only: [:new, :create]
   before_action :ensure_eventrinfo_filled_out, only: [:join]
   before_action :load_user, only: [:create, :destroy, :edit, :update, :join]
