893c84d30b45a16b0f538dfe346e083af2b6dd25
ilia24 <iliademertchian24@gmail.com>
2017-05-25 00:34:20 -0400

    Move private chat actioncable code to rooms.js in setup for multi-actioncable sidebar logic

--- app/assets/javascripts/channels/rooms.js
+++ app/assets/javascripts/channels/rooms.js
@@ -1,6 +1,7 @@
 $(function() {
-function LoadChat() {
 
+//THIS FUNCTION IS RESPONSIBLE FOR LOADING GROUP CHATS
+function LoadChat() {
   // this code scrolls the chatbox to the bottom
   var messages, messages_to_bottom;
   messages = $('#messages');
@@ -49,11 +50,62 @@ function LoadChat() {
   }
 };
 
+//THIS FUNCTION IS RESPONSIBLE FOR LOADING PRIVATE CHATS//
+function LoadPrivateChat() {
+  // this code scrolls the chatbox to the bottom
+  var messages, messages_to_bottom;
+  messages = $('#convo');
+  if ($('#convo').length > 0) {
+    messages_to_bottom = function() {
+      return messages.scrollTop(messages.prop("scrollHeight"));
+    };
+    messages_to_bottom();
+
+//This code creates an AC subscription to the specific group channel, and establishes base functions
+    App.private_chat = App.cable.subscriptions.create({
+      channel: "ConvoChannel",
+      conversation_id: convo.dataset.conversationId
+    }, {
+      connected: function() {
+        console.log('PCHAT: connected to ' + convo.dataset.conversationId)
+      },
+      disconnected: function() {
+        console.log('PCHAT: disconnected from ' + convo.dataset.conversationId)
+      },
+      received: function(data) {
+        messages.append(data['chat']);
+        return messages_to_bottom();
+      },
+      send_chat: function(textdata, convo_id) {
+        return this.perform('send_chat', {
+          body: textdata,
+          conversation_id: convo_id
+        });
+      }
+    });
+
+    // this is responsible for actually sending a message
+    return $('#new_personal_message').submit(function(e) {
+      var $this, textarea;
+      $this = $(this);
+      textarea = $this.find('#personal_message_body');
+      messages = $('#convo');
+      if ($.trim(textarea.val()).length > 1) {
+        App.private_chat.send_chat(textarea.val(), convo.dataset.conversationId);
+        textarea.val('');
+      }
+      e.preventDefault();
+      return false;
+    });
+  }
+};
+
 if (document.cookie === "") {
   console.log('user not logged in, not launching chat cable')
   return
 } else {
   LoadChat();
+  LoadPrivateChat();
   $("#eventdata").remove();
 };
 
