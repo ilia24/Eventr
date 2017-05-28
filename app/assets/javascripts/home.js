c1530beb7e97de61be8be32d82c8559dfe7c2ee8
Brian Du <brian.bh.du@gmail.com>
2017-05-28 18:30:25 -0400

    Replace carousel nav arrows if mobile screen size

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,8 +1,17 @@
 $(document).ready(function () {
+
   $('.carousel_item').hide().fadeIn("slow");
   $('.carousel_item').toggleClass('carousel_item_slide_up');
   $('.home_event_name').hide().fadeIn("slow");
   $('.home_event_name').toggleClass('home_event_name_slide');
   $('#home_event_buy_btn').hide().fadeIn("slow");
   $('#home_event_buy_btn').toggleClass('home_event_buy_slide');
+
+  if ($(window).width() <= 414) {
+
+    $(".fa.fa-angle-right").removeClass('fa fa-angle-right fa-3x').addClass('fa fa-angle-right fa-2x');
+    $(".fa.fa-angle-left").removeClass('fa fa-angle-left fa-3x').addClass('fa fa-angle-left fa-2x');
+
+  };
+
 });
