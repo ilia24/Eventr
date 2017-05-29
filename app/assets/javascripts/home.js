f670e859daa25664114501c5ade6359b8f24599b
Brian Du <brian.bh.du@gmail.com>
2017-05-28 22:27:48 -0400

    Styled mobile version of carousel

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -7,10 +7,17 @@ $(document).ready(function () {
   $('#home_event_buy_btn').hide().fadeIn("slow");
   $('#home_event_buy_btn').toggleClass('home_event_buy_slide');
 
+  // If iPhone 6s Plus or smaller change carousel arrow size
   if ($(window).width() <= 414) {
 
     $(".fa.fa-angle-right").removeClass('fa fa-angle-right fa-3x').addClass('fa fa-angle-right fa-2x');
     $(".fa.fa-angle-left").removeClass('fa fa-angle-left fa-3x').addClass('fa fa-angle-left fa-2x');
+
+    $(function() {
+      $('h1').each(function(i, em) {
+         $(em).replaceWith('<h4>'+$(em).html()+'</h4>');
+      });
+    });
 
   };
 
